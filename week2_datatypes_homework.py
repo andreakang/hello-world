2723fce858cc71f3f9f7f376d71c12721f08a41a
Andrea Kang <andreakang@users.noreply.github.com>
2017-10-09 23:25:38 -0400

    Delete week2_datatypes_homework.py

--- week2_datatypes_homework.py
+++
@@ -1,4 +0,0 @@
-light = true # the light in my room is on, not off
-mikeysFries = ["fries", "cheese fries", "chili cheese fries", "sweet potato fries"] # all the varieties of fries they offer at Mikey's Burger next to my apartment
-costCheeseFries = 5.45 # the price of cheese fries at Mikey's
-acUnit = {"color": "light grey", "fanSpeed": "low", "height": 18, "width" : 24, "fanCoolness": 2} # properties of my ac unit
